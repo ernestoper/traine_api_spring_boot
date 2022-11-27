d9592bcf1c06e8cd03baa61102a2addc75a84f72
ernestoper <alejojus@gmail.com>
2022-11-27 18:05:50 -0300

    Greeting java with spring boot

---
+++ traine_api_greeting/src/main/java/br/com/nazcatech/StartUp.java
@@ -0,0 +1,13 @@
+package br.com.nazcatech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUp.class, args);
+	}
+
+}
